e3c86ba3e8dafe250911bb90741fda3d73f549b2
wobblycode <cloud@kloudsanta.com>
2025-08-14 18:01:12 -0400

    Update providers.tf

--- modules/lesson_01/providers.tf
+++ modules/lesson_01/providers.tf
@@ -1,1 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.9.0"
+    }
+  }
+}
 
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
